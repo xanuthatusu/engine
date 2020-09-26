720a9b07c7ecdf6ce34a1b0253ea597340c85a78
Xanuthatusu <anthony@tepia.net>
2020-09-26 10:11:28 -0600

    Working demo / understanding of vertex atrrib pointer and how to draw points

--- bin/media/shaders/triangles.vert
+++ bin/media/shaders/triangles.vert
@@ -1,7 +1,7 @@
 #version 450 core
 
-layout (location = 0) in vec4 vPosition;
+layout (location = 0) in vec2 vPosition;
 
 void main() {
-  gl_Position = vPosition;
+  gl_Position = vec4(vPosition.x, vPosition.y, 0.0, 1.0);
 }
--- src/engine/framework/Draw.cpp
+++ src/engine/framework/Draw.cpp
@@ -3,6 +3,8 @@
 
 #include <engine/framework/Draw.h>
 
+#include <iostream>
+
 void Draw::AddShape(Shape* shape) {
   shapes.push_back(shape);
 }
@@ -11,10 +13,12 @@ void Draw::UpdateScreen() {
   static const float black[] = { 0.0f, 0.0f, 0.0f, 0.0f };
   static const float red[] = { 1.0f, 0.0f, 0.0f, 0.0f };
 
+  std::vector<GLfloat> vertices = getVertices();
+
   glClearBufferfv(GL_COLOR, 0, black);
 
   glBindVertexArray(VAOs[Triangles]);
-  glDrawArrays(GL_TRIANGLES, 0, 3);
+  glDrawArrays(GL_TRIANGLES, 0, vertices.size() / 2);
 
   glFlush();
 }
@@ -27,20 +31,27 @@ Draw::Draw() {
 
   glGenBuffers(NumBuffers, Buffers);
   glBindBuffer(GL_ARRAY_BUFFER, Buffers[ArrayBuffer]);
-  glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vertices[0]), vertices.data(), GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
 
-  glVertexAttribPointer(vPosition, 2, GL_FLOAT, GL_FALSE, sizeof(vertices[0]), 0);
+  glVertexAttribPointer(vPosition, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 2, 0);
   glEnableVertexAttribArray(vPosition);
 }
 
 std::vector<GLfloat> Draw::getVertices() {
   std::vector<GLfloat> vertices;
 
-  for (int i = 0; i < 3; i++) {
-    GLfloat v = 0.3f * (i+1);
-    vertices.push_back(v);
-    vertices.push_back(v);
-  }
+  GLfloat v = 0.5f;
+  vertices.push_back(v);
+  v = 0.1f;
+  vertices.push_back(v);
+  v = -0.1f;
+  vertices.push_back(v);
+  v = -0.7f;
+  vertices.push_back(v);
+  v = -0.5f;
+  vertices.push_back(v);
+  v = 0.3f;
+  vertices.push_back(v);
 
   return vertices;
 }
